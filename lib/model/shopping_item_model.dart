1eb0212c0abd8a474df66fb1281cf9623685312a
seoyoon513 <syoonlee513@gmail.com>
2023-03-21 17:16:10 +0900

    [feat] model에 copyWith() 함수 설계

--- lib/model/shopping_item_model.dart
+++ lib/model/shopping_item_model.dart
@@ -1,10 +1,13 @@
 class ShoppingItemModel {
   // 이름
   final String name;
+
   // 개수
   final int quantity;
+
   // 구매했는지
   final bool hasBought;
+
   // 매운지
   final bool isSpicy;
 
@@ -14,4 +17,20 @@ class ShoppingItemModel {
     required this.hasBought,
     required this.isSpicy,
   });
+
+  ShoppingItemModel copyWith({
+    // optional로 값을 받고
+    String? name,
+    int? quantity,
+    bool? hasBought,
+    bool? isSpicy,
+  }) {
+    // 입력된 값이 있으면 입력된 값, 없으면(null이면) 기존 값 대입
+    return ShoppingItemModel(
+      name: name ?? this.name,
+      quantity: quantity ?? this.quantity,
+      hasBought: hasBought ?? this.hasBought,
+      isSpicy: isSpicy ?? this.isSpicy,
+    );
+  }
 }
